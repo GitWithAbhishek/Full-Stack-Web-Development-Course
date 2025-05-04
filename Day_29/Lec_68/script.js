af0e6259c53d67b54559d64f806327a33ee419b7
Abhishek Pandey <abhishekpandeylko2004@gmail.com>
2025-05-04 13:11:12 +0530

    Completed Day 29

---
+++ Day_29/Lec_68/script.js
@@ -0,0 +1,7 @@
+console.log("Lec 68");
+
+let boxes = document.getElementsByClassName("box")
+console.log(boxes)
+boxes[2].style.backgroundColor = "blue"
+
+document.getElementById("fontorange").style.color="orange"
---
+++ Day_29/Lec_70/script.js
@@ -0,0 +1,13 @@
+let colors=["red","green","Blue","Brown","black","grey","orange","purple"]
+
+let box = document.querySelectorAll(".box")
+
+let size = colors.length
+
+box.forEach(element => {
+
+    let random=Math.floor(Math.random()*size)
+
+    element.style.backgroundColor=colors[random]
+
+});
